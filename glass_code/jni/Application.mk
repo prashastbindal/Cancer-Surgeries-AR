ac97e53971f0feee2379c6e08fc701cc7f6f2418
Prashast Bindal <prashast.bindal@rash-mac.local>
2018-07-01 01:11:36 +0530

    edited gitignore

--- glass_code/jni/Application.mk
+++
@@ -1,7 +0,0 @@
-# Building OpenCV for Static Library
-APP_STL := gnustl_static
-APP_CPPFLAGS := -frtti -fexceptions
-
-# Google Glass is armeabi-v7a
-APP_ABI := armeabi-v7a
-APP_PLATFORM := android-19
